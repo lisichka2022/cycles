6250bb69407d56ae1daf51903da6745687667577
lisichka2022 <99811204+lisichka2022@users.noreply.github.com>
2022-12-22 10:43:36 +0200

    Add files via upload

---
+++ first.js
@@ -0,0 +1,33 @@
+'use strict'
+//1
+for (let i = 1; i <= 100; i++) {
+	console.log(i );
+}
+
+//2
+for (let i = 100; i >= 1; i--) {
+console.log(i );}
+
+    //3
+
+for(let i=2;i<=100;i+=2){
+console.log(i)}
+
+    //4
+
+ const arr = [1, 2, 3, 4, 5];
+let result=0;
+for(let i=0; i<arr.length;i++){
+    result+=arr[i];}
+    console.log(result);
+
+    //5
+const arr = [1, 2, 3, 4, 5];
+let result=0;
+for(let i=0; i<arr.length;i++){
+result+=arr[i] * arr[i];}
+
+console.log(result);
+
+  
+
